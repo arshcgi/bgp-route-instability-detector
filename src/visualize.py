05ddbbc9ad98e62f4177b7167dececf4d281c4b7
arshcgi <ciscoarsh@gmail.com>
2026-03-12 23:52:49 +0000

    Update visualize.py

--- src/visualize.py
+++ src/visualize.py
@@ -1,1 +1,6 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
 
+sns.histplot(grouped['update_count'], log_scale=True)
+plt.title("Distribution of BGP Update Counts per Prefix")
+plt.show()
